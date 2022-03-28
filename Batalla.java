e13e2ffea91275c7b47b41081223c1d4701b19ef
Luis Valerio Fuentes <100722753+luisvalerio2000@users.noreply.github.com>
2022-03-27 21:12:54 -0600

    Create Batalla.java

---
+++ Batalla.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto01_ipc1a;
+
+/**
+ *
+ * @author fuent
+ */
+public class Batalla {
+    public int tier=0;
+    public int cantRondas=0;
+    
+    
+    //public void batalla
+    
+    
+}
